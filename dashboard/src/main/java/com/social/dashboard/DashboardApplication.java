41a4826bb73975fb0abc01b30f0818ae2d8492a1
Ambrish <ambrishdadhwal31@gmail.com>
2023-02-27 15:14:06 +0530

    Spring Config server & Client

--- dashboard/src/main/java/com/social/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/com/social/dashboard/DashboardApplication.java
@@ -1,13 +1,35 @@
 package com.social.dashboard;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DashboardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DashboardApplication.class, args);
     }
 
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${name:Config Server is not working. Please check...}")
+    private String name;
+
+    @GetMapping("/msg/")
+    public String getMsg()
+    {
+        return name;
+    }
 }
